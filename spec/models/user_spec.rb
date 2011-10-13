0ca71e5229c85f20ff5a23070168b41158611854
Arzumy MD <hello@ar.zu.my>
2011-10-13 13:26:00 +0800

    spec for User.dummy_create()

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,22 @@
 require 'spec_helper'
 
 describe User do
+  context "validation" do
+    describe "fullname" do
+      it "invalid without fullname" do
+        user = User.new(fullname: nil, email: 'user02@example.com', password: 'password', password_confirmation: 'password')
+        user.should have(1).error_on(:fullname)
+      end
+    end
+
+    describe "email" do
+      it "downcase email" do
+        user = User.create!(fullname: "downcase", email: 'DOWNCASE@example.com', password: 'password', password_confirmation: 'password')
+        user.email.should == 'downcase@example.com'
+      end
+    end
+  end
+
   describe "#friends" do
     it "returns users who are friends" do
       user01 = users(:user01)
@@ -18,6 +34,22 @@ describe User do
     context "with one friend" do
       it "returns array of user id" do
         users(:user01).friend_ids.should == [users(:user02).id]
+      end
+    end
+  end
+
+  describe "User.dummy_create()" do
+    context "valid email" do
+      it "creates new user" do
+        User.dummy_create('thisisnew@example.com').should be_valid
+      end
+    end
+
+    context "invalid email" do
+      it "raise exception" do
+        expect do
+          User.dummy_create('notanemail')
+        end.to raise_exception(ActiveRecord::RecordInvalid)
       end
     end
   end
