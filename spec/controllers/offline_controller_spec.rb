7172c22d5a4783caa0e0fa0a727533ccd83817bf
Arzumy MD <hello@ar.zu.my>
2011-10-25 18:03:47 +0800

    first pass at appcache

---
+++ spec/controllers/offline_controller_spec.rb
@@ -0,0 +1,32 @@
+require "spec_helper"
+
+describe OfflineController do
+  render_views
+
+  describe "GET show" do
+    it "renders content-type properly" do
+      get :show
+      response.content_type.should == 'text/cache-manifest'
+    end
+
+    it "renders show template" do
+      get :show
+      response.should render_template("offline/show")
+    end
+
+    it "lists assets" do
+      get :show
+      response.body.should match(/bg\.png/)
+      response.body.should match(/logo\.png/)
+      response.body.should match(/#{Rails.application.assets.find_asset('application.css').digest_path}/)
+      response.body.should match(/#{Rails.application.assets.find_asset('application.js').digest_path}/)
+    end
+
+    it "includes current user" do
+      user = users(:user01)
+      sign_in user
+      get :show
+      response.body.should match(/#{user.friendly_id}/)
+    end
+  end
+end
